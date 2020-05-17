1c1057f3703929b6d2c277de18298b60ae57d917
Konstantin Trushin <konstantin.trushin@gmail.com>
2020-05-17 14:30:28 +0300

    Fix `clang-tidy.sh` script

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -5,7 +5,7 @@ set -e
 clang_exe=$CLANG
 clang_tidy_exe=$CLANG_TIDY
 if [ -z "$clang_exe" ]; then
-  clang_tidy='clang++'
+  clang_exe='clang++'
 fi
 if [ -z "$clang_tidy_exe" ]; then
   clang_tidy_exe='clang-tidy'
